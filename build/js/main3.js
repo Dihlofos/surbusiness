8177cd8b2f3e563b75db35b1b3038043da38f993
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2023-02-20 18:31:26 +0300

    добавил анимации на первый экран

--- build/js/main3.js
+++ build/js/main3.js
@@ -1,5 +1,34 @@
 "use strict";
 (function () {
+
+  const container = document.querySelector('.js-animaicons');
+  const columns = container.querySelectorAll('.js-animaicons-column');
+
+  columns.forEach((column)=>{
+    setInterval(()=>{
+      changeVisibility(column);
+    }, 1200);
+
+  })
+
+
+
+  function changeVisibility(column) {
+    const shown = column.querySelector('.is-shown');
+    shown.classList.remove('is-shown');
+    if (shown.nextSibling !== null) {
+      shown.nextSibling.classList.add('is-shown');
+    } else {
+      column.children[0].classList.add('is-shown');
+
+    }
+  }
+
+
+})();
+
+"use strict";
+(function () {
   // init controller
   const controller = new ScrollMagic.Controller();
   const vw = window.innerWidth;
---
+++ source/js/main/animaicons.js
@@ -0,0 +1,28 @@
+"use strict";
+(function () {
+
+  const container = document.querySelector('.js-animaicons');
+  const columns = container.querySelectorAll('.js-animaicons-column');
+
+  columns.forEach((column)=>{
+    setInterval(()=>{
+      changeVisibility(column);
+    }, 1200);
+
+  })
+
+
+
+  function changeVisibility(column) {
+    const shown = column.querySelector('.is-shown');
+    shown.classList.remove('is-shown');
+    if (shown.nextSibling !== null) {
+      shown.nextSibling.classList.add('is-shown');
+    } else {
+      column.children[0].classList.add('is-shown');
+
+    }
+  }
+
+
+})();
