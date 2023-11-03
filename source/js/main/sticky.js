2db80aab7afb11723f82ef72ba715a6b9840de37
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2023-11-03 17:13:53 +0400

    Added nav blinks

--- source/js/main/sticky.js
+++ source/js/main/sticky.js
@@ -1,6 +1,8 @@
 "use strict";
 (function () {
   var sticky = document.getElementsByClassName("js-sticky")[0];
+  const links = document.querySelectorAll(".js-nav");
+  if (!sticky || !links) return;
   var stickyAnchor = sticky.parentNode;
   var state = false;
 
@@ -16,10 +18,36 @@
       sticky.classList.remove("is-sticky");
       state = false;
     }
+
+    links.forEach((link) => {
+      const item = document.getElementById(link.dataset.link);
+      if (isInViewport(item)) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
   };
 
   window.addEventListener("scroll", updateSticky);
   window.addEventListener("resize", updateSticky);
 
   updateSticky();
+
+  function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+
+    const myElementHeight = el.offsetHeight - 120;
+    const myElementWidth = el.offsetWidth - 120;
+    return (
+      rect.top >= -myElementHeight &&
+      rect.left >= -myElementWidth &&
+      rect.right <=
+        (window.innerWidth || document.documentElement.clientWidth) +
+          myElementWidth &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) +
+          myElementHeight
+    );
+  }
 })();
